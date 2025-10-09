7949e30e392309f6ca71ac3fcf3f566e7057b1ca
Francisco Boldt <fboldt@gmail.com>
2025-10-09 13:19:55 -0300

    Add new experiment testing script and remove old dataset implementation

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
-from pprint import pprint
-from datasets.cwru.utils import get_code_from_faulty_bearing, get_list_of_folds_rauber_loca_et_al
-from datasets.utils import get_X_y, get_train_test_split
+from experiments import test_experiments
 
 if __name__ == "__main__":
-    faulty_bearing='Drive End'
-    sample_rate='12000'
-    folds = get_list_of_folds_rauber_loca_et_al(faulty_bearing=faulty_bearing, sample_rate=sample_rate)
-    channel = get_code_from_faulty_bearing(faulty_bearing)
-    list_of_X_y = []
-    for fold in folds:
-        # pprint(fold)
-        print(f"Number of acquisitions: {len(fold)}")
-        X, y = get_X_y(fold, raw_dir_path="raw_data/cwru", channel=channel)
-        list_of_X_y.append((X, y))
-    for i in range(len(folds)):
-        print(f"{'#'*3} Fold {i}:")
-        X_train, y_train, X_test, y_test = get_train_test_split(list_of_X_y, test_fold_index=i)
-        print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-        # print(f"Unique classes in y_train: {set(y_train)}")
-        print(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
-    #     print(f"Unique classes in y_test: {set(y_test)}")
+    test_experiments()
