0bc531b3285634e8fbbd8287efeea6cf3879d078
Francisco Boldt <fboldt@gmail.com>
2026-02-05 16:10:46 -0300

    Remove obsolete estimators and feature extraction classes; implement new feature extraction pipeline in main execution flow

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from pprint import pprint
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
-from estimator.SFRF import SFRF as Estimator
+from sklearn.pipeline import make_pipeline
+from feature.extraction import WaveletFeatures
 
-model = Estimator()
+model = make_pipeline(WaveletFeatures(), RandomForestClassifier())
 
 def f1_macro(y_true, y_pred):
     return f1_score(y_true, y_pred, average='macro')
 list_of_metrics = [accuracy_score, f1_macro, confusion_matrix]
-
 
 if __name__ == "__main__":
     #'''
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     print("Scores for papers experiment:")
     pprint(scores)
     ''' 
-    from dataset.cwru.sehri_et_al import run_proposed_experiment
-    run_proposed_experiment(model, list_of_metrics)
+    from dataset.cwru.sehri_et_al import run_papers_inspired_experiment
+    from assesment.nested import performance
+    run_papers_inspired_experiment(model, list_of_metrics, performance)
     #'''
