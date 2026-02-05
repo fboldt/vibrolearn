5e3df6044d797e51599d664f9703c54bcf60180a
Francisco Boldt <fboldt@gmail.com>
2026-02-05 20:51:25 -0300

    Implement nested cross-validation and enhance get_best_params function in utils.assessment.py

--- utils/assesment.py
+++ utils/assesment.py
@@ -1,4 +1,5 @@
 from dataset.utils import concatenate_data
+from utils.metrics import f1_macro
 
 def train_test_split(list_of_X_y, test_fold_index):
     X_test, y_test = list_of_X_y[test_fold_index]
@@ -21,3 +22,32 @@ def cross_validation(model, list_of_X_y, list_of_metrics):
         scores = holdout(model, list_of_X_y, test_fold_index=i, list_of_metrics=list_of_metrics)
         scores_per_fold.append(scores)
     return scores_per_fold
+
+def get_best_params(model, params, list_of_X_y):
+    best_score = 0
+    best_params = None
+    for param_combination in params:
+        model.set_params(**param_combination)
+        scores_per_fold = cross_validation(model, list_of_X_y, list_of_metrics=[f1_macro])
+        avg_score = sum([scores['f1_macro'] for scores in scores_per_fold]) / len(scores_per_fold)
+        if avg_score > best_score:
+            best_score = avg_score
+            best_params = param_combination
+    return best_params
+
+def nested_cross_validation(model, params, list_of_X_y, list_of_metrics):
+    n_folds = len(list_of_X_y)
+    scores_per_fold = []
+    for test_fold_out_index in range(n_folds):
+        X_test_out, y_test_out = list_of_X_y[test_fold_out_index]
+        list_of_X_y_out =  [list_of_X_y[i] for i in range(len(list_of_X_y)) if i != test_fold_out_index]
+        best_params = get_best_params(model, params, list_of_X_y_out)
+        model.set_params(**best_params)
+        model.fit(*concatenate_data(list_of_X_y_out))
+        y_test_pred = model.predict(X_test_out)
+        scores = {}
+        for metric in list_of_metrics:
+            scores[metric.__name__] = metric(y_test_out, y_test_pred)
+        scores_per_fold.append(scores)
+    return scores_per_fold
+    
